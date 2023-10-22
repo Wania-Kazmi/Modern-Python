190187d3f56184743d582d74930d39ce1026d91d
Wania-Kazmi <waniakz019@gmail.com>
2023-10-22 21:19:12 +0500

    class1 commit

---
+++ class01/class.py
@@ -0,0 +1,19 @@
+# print("hello world")
+# print("Pakistan")
+# print("Zindabad")
+# name:str = "wania"
+# print("Name is: ",name)
+# print("Type is: ", type(name))
+# print("ID is: ", id(name))
+# print("String method attribute: ", dir(str)) - same like below
+# print("Name attribute: ", dir(name))
+# print("Name attributes are: ", [i for i in dir(name) if "__" not in i])
+
+from typing import Any
+
+
+# valueStr : str = 700
+# print(valueStr)
+
+valueStr1 : Any = 700
+print(valueStr1)
