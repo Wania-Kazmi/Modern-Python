11a0babe7666a6dc3702ebed5ad37aff18aa826c
Wania-Kazmi <waniakz019@gmail.com>
2023-10-27 08:29:47 +0500

    class6 updated

--- class06/class6.py
+++ class06/class6.py
@@ -1,7 +1,42 @@
-names : str = 23
-print(names)
+# grade: Union[int, float] = 7.0
+# print(grade)
 
-from typing import Union
+from typing import Union 
 
-grade: Union[int, float] = 7.0
-print(grade)
+# alias
+PerType = Union[float, int]
+
+percentages: list[PerType] = [88, 82.3, 90, 54, 65.72]
+
+grades: list[str] = []
+
+for student_percentage in percentages: 
+    if student_percentage >= 90:
+        grade = "A+"
+        grades.append(grade)
+    elif student_percentage >= 85:
+        grade = "A"
+        grades.append(grade)
+    elif student_percentage >= 80:
+        grade = "B+"
+        grades.append(grade)
+    elif student_percentage >= 75:
+        grade = "B"
+        grades.append(grade)
+    elif student_percentage >= 70:
+        grade = "C+"
+        grades.append(grade)
+    elif student_percentage >= 65:
+        grade = "C"
+        grades.append(grade)
+    elif student_percentage >= 60:
+        grade = "D"
+        grades.append(grade)
+    else:
+        grade = "Fail"
+        grades.append(grade)
+print(grades)
+
+#zipping
+
+print(list(zip(percentages, grades)))
