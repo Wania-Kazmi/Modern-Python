3538baddd9a253c0f2114543ef67a52d1fe0b8d7
Wania-Kazmi <waniakz019@gmail.com>
2023-10-28 21:12:00 +0500

    class9 input and iteration on loop

---
+++ class09/class9.py
@@ -0,0 +1,11 @@
+import sys
+
+print("line1")
+print("line2")
+
+print(type(sys.argv)) #recieve an object (that is unlimited) from console - and came here in form of iterative data type
+print(sys.argv) #recieve an object (that is unlimited) from console - and came here in form of iterative data type
+
+# we run this command on anaconda prompt - python class9.py 1 2 3 ("a","b","c")
+
+# sys.argv - it has list data type by default - at 0 index there is file name and all the values are custom user define
